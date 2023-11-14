1ca3d190d25f1143187ebe46a83acab6d0187d69
me <znamsiparolata4esno@abv.bg>
2023-11-14 21:12:36 +0200

    update

--- lib_character_map.py
+++ lib_character_map.py
@@ -3,4 +3,6 @@ CHARACTER_MAP = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ /:?[theta].,()'
 
 # NOTE if you are to add new characters append them to the end so that we don't loose backwards compatibility
 
+# NOTE make sure that any characters that need to be indexed stay at the beginning so the indexes don't get fucked by the multibyte chars
+
 # TODO? lower case letters are missing
